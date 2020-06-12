5c798132bde3889ec7e98d5fbb3f2da9a755e688
nickas101 <nikolai.astashkin@rakon.com>
2020-06-12 13:11:11 +1200

    first commit

---
+++ main.py
@@ -0,0 +1,25 @@
+from app import app
+from waitress import serve
+from paste.translogger import TransLogger
+import logging.config
+import logging
+from datetime import datetime
+
+
+# LOG_FILENAME = 'app/temp_files/requests.log'
+# logging.basicConfig(filename=LOG_FILENAME,level=logging.INFO)
+# logging.info("\n")
+# logging.info("*** The application started: " + str(datetime.now()))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    #serve(TransLogger(app, logger_name='lta', setup_console_handler=False), host='127.0.0.1', port=5003)
+    #serve(TransLogger(app, logger_name='lta', setup_console_handler=False), host='172.20.7.226', port=8081)
+
+
+
+    # serve(app, host='127.0.0.1', port=5000)
+    #serve(app, host='172.20.7.226', port=8080)
+    #app.run('0.0.0.0', 8080)
+    # app.run('0.0.0.0', 80)
